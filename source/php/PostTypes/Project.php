c10e738e9abb2576d6312ec34dee2d7932b3e787
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-04-01 17:13:32 +0200

    Register taxonomies

--- source/php/PostTypes/Project.php
+++ source/php/PostTypes/Project.php
@@ -32,15 +32,52 @@ class Project
             $args
         );
 
-        // Status taxonomy
+        // Statuses
         $postType->addTaxonomy(
             'status',
             __('Status', APIPROJECTMANAGER_TEXTDOMAIN),
             __('Statuses', APIPROJECTMANAGER_TEXTDOMAIN),
-            array(
-                'hierarchical'      => true,
-                'show_admin_column' => true,
-                )
+            array('hierarchical' => true)
+        );
+
+        // Technologies
+        $postType->addTaxonomy(
+            'technology',
+            __('Technology', APIPROJECTMANAGER_TEXTDOMAIN),
+            __('Technologies', APIPROJECTMANAGER_TEXTDOMAIN),
+            array('hierarchical' => true)
+        );
+
+        // Sectors
+        $postType->addTaxonomy(
+            'sector',
+            __('Sector', APIPROJECTMANAGER_TEXTDOMAIN),
+            __('Sectors', APIPROJECTMANAGER_TEXTDOMAIN),
+            array('hierarchical' => true)
+        );
+
+        // Organisations
+        $postType->addTaxonomy(
+            'organisation',
+            __('Organisation', APIPROJECTMANAGER_TEXTDOMAIN),
+            __('Organisations', APIPROJECTMANAGER_TEXTDOMAIN),
+            array('hierarchical' => true)
+        );
+
+        // Global goals
+        $postType->addTaxonomy(
+            'global_goal',
+            __('Global goal', APIPROJECTMANAGER_TEXTDOMAIN),
+            __('Global goals', APIPROJECTMANAGER_TEXTDOMAIN),
+            array('hierarchical' => true)
+        );
+
+        // Categories
+        $postType->addTaxonomy(
+            'category',
+            __('Category', APIPROJECTMANAGER_TEXTDOMAIN),
+            __('Categories', APIPROJECTMANAGER_TEXTDOMAIN),
+            array('hierarchical' => true)
         );
     }
 }

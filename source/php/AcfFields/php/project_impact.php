52cbc8cc5ce16abfa8fea4db9b525e6178407479
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-18 10:36:04 +0100

    Add fields for impact

---
+++ source/php/AcfFields/php/project_impact.php
@@ -0,0 +1,113 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5fb4e0ff4c111',
+    'title' => __('Project impact', 'api-project-manager'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5fb4e11cb4514',
+            'label' => __('Impact Goals', 'api-project-manager'),
+            'name' => 'impact_goals',
+            'type' => 'repeater',
+            'instructions' => '',
+            'required' => 1,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'collapsed' => '',
+            'min' => 1,
+            'max' => 0,
+            'layout' => 'table',
+            'button_label' => __('Add goal', 'api-project-manager'),
+            'sub_fields' => array(
+                0 => array(
+                    'key' => 'field_5fb4e1c1b4515',
+                    'label' => __('Expected output', 'api-project-manager'),
+                    'name' => 'impact_goal',
+                    'type' => 'text',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                ),
+                1 => array(
+                    'key' => 'field_5fb4e6a8b4517',
+                    'label' => __('Comment', 'api-project-manager'),
+                    'name' => 'impact_goal_comment',
+                    'type' => 'textarea',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => array(
+                        0 => array(
+                            0 => array(
+                                'field' => 'field_5fb4e1d5b4516',
+                                'operator' => '==',
+                                'value' => '1',
+                            ),
+                        ),
+                    ),
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'maxlength' => '',
+                    'rows' => 1,
+                    'new_lines' => '',
+                ),
+                2 => array(
+                    'key' => 'field_5fb4e1d5b4516',
+                    'label' => __('Completed', 'api-project-manager'),
+                    'name' => 'impact_goal_completed',
+                    'type' => 'true_false',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '10',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'message' => '',
+                    'default_value' => 0,
+                    'ui' => 0,
+                    'ui_on_text' => '',
+                    'ui_off_text' => '',
+                ),
+            ),
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'project',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
+}

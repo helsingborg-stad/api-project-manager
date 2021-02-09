535b4fd2f26aed1e42ef298237c9e86181c15681
nRamstedt <nikolas.ramstedt@gmail.com>
2021-02-09 14:43:43 +0100

    Add: fields for resident involvement & translation

---
+++ source/php/AcfFields/php/project_resident_involvement.php
@@ -0,0 +1,67 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_6022843249a67',
+    'title' => __('Resident involvement', 'api-project-manager'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_6022843cf2e03',
+            'label' => __('Resident involvement', 'api-project-manager'),
+            'name' => 'resident_involvement',
+            'type' => 'repeater',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'collapsed' => '',
+            'min' => 0,
+            'max' => 0,
+            'layout' => 'block',
+            'button_label' => '',
+            'sub_fields' => array(
+                0 => array(
+                    'key' => 'field_60228444f2e04',
+                    'label' => __('Description', 'api-project-manager'),
+                    'name' => 'description',
+                    'type' => 'textarea',
+                    'instructions' => '',
+                    'required' => 1,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'maxlength' => '',
+                    'rows' => 2,
+                    'new_lines' => '',
+                ),
+            ),
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'project',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}

9386647e4fbe53828ee7040a746d71bfaa66bbd9
nRamstedt <nikolas.ramstedt@gmail.com>
2021-02-25 16:35:19 +0100

    Add field for URL

---
+++ source/php/AcfFields/php/tax_focal_point_meta.php
@@ -0,0 +1,43 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_6037bb3ded5e1',
+    'title' => __('Focal Point Meta', 'api-project-manager'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_6037bb4e80edd',
+            'label' => __('URL', 'api-project-manager'),
+            'name' => 'url',
+            'type' => 'url',
+            'instructions' => '',
+            'required' => 1,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'taxonomy',
+                'operator' => '==',
+                'value' => 'focal_point',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}

364e942aa7317b87afd37f4144a8021389087650
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-06-07 10:57:24 +0200

    Revert un intended changes

--- source/php/AcfFields/php/project_editor.php
+++ source/php/AcfFields/php/project_editor.php
@@ -1,7 +1,9 @@
 <?php 
 
-if (function_exists('acf_add_local_field_group')) {
-    acf_add_local_field_group(array(
+
+if (function_exists('acf_add_local_field_group')) {
+
+    acf_add_local_field_group(array(
     'key' => 'group_5ec7baa64e985',
     'title' => __('Project Editor', 'api-project-manager'),
     'fields' => array(
@@ -27,28 +29,6 @@
             'return_format' => 'id',
             'multiple' => 0,
         ),
-        1 => array(
-            'key' => 'field_76a1n2gkb',
-            'label' => __('Platform', 'api-project-manager'),
-            'name' => 'platform',
-            'type' => 'taxonomy',
-            'instructions' => '',
-            'required' => 0,
-            'conditional_logic' => 0,
-            'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
-            ),
-            'taxonomy' => 'platform',
-            'field_type' => 'multi_select',
-            'allow_null' => 0,
-            'add_term' => 0,
-            'save_terms' => 1,
-            'load_terms' => 0,
-            'return_format' => 'id',
-            'multiple' => 0,
-        ),
     ),
     'location' => array(
         0 => array(
@@ -65,7 +45,8 @@
     'label_placement' => 'top',
     'instruction_placement' => 'label',
     'hide_on_screen' => '',
-    'active' => true,
+    'active' => 1,
     'description' => '',
 ));
-}
+
+}

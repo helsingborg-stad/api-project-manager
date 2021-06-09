b4145cd1f04c434e667331bf46483ca0fc2ab41e
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-06-09 08:43:55 +0200

    add platform feature fields

---
+++ source/php/AcfFields/php/platform_features.php
@@ -0,0 +1,105 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_60c05e6dedc15',
+    'title' => __('Platform Features', 'api-project-manager'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_60c05e72272b9',
+            'label' => __('Features', 'api-project-manager'),
+            'name' => 'platform_features',
+            'type' => 'repeater',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'collapsed' => 'field_60c05e8a272ba',
+            'min' => 0,
+            'max' => 0,
+            'layout' => 'row',
+            'button_label' => '',
+            'sub_fields' => array(
+                0 => array(
+                    'key' => 'field_60c05e8a272ba',
+                    'label' => __('Title', 'api-project-manager'),
+                    'name' => 'title',
+                    'type' => 'text',
+                    'instructions' => '',
+                    'required' => 1,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                ),
+                1 => array(
+                    'key' => 'field_60c060ab5b520',
+                    'label' => __('Category', 'api-project-manager'),
+                    'name' => 'category',
+                    'type' => 'text',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                ),
+                2 => array(
+                    'key' => 'field_60c05e90272bb',
+                    'label' => __('Content', 'api-project-manager'),
+                    'name' => 'content',
+                    'type' => 'textarea',
+                    'instructions' => '',
+                    'required' => 1,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'maxlength' => 200,
+                    'rows' => '',
+                    'new_lines' => '',
+                ),
+            ),
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'platform',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
+}

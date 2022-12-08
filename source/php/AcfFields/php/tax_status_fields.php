ae337296043957c554de71a11b294cb952ed2e2b
Anna Johansson <anna@thehappyhipster.se>
2022-12-08 14:27:41 +0100

    Added field for lessons_learned_is_obligatory on status taxonomy

--- source/php/AcfFields/php/tax_status_fields.php
+++ source/php/AcfFields/php/tax_status_fields.php
@@ -9,6 +9,7 @@
             'key' => 'field_5fb7d4611f925',
             'label' => __('Progress value', 'api-project-manager'),
             'name' => 'progress_value',
+            'aria-label' => '',
             'type' => 'range',
             'instructions' => '',
             'required' => 1,
@@ -24,6 +25,26 @@
             'step' => 1,
             'prepend' => '',
             'append' => __('%', 'api-project-manager'),
+        ),
+        1 => array(
+            'key' => 'field_6391e4303f343',
+            'label' => __('Requires "Lessons learned"', 'api-project-manager'),
+            'name' => 'lessons_learned_is_obligatory',
+            'aria-label' => '',
+            'type' => 'true_false',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'message' => __('The lessons learned field is obligatory when this status is active', 'api-project-manager'),
+            'default_value' => 0,
+            'ui_on_text' => __('Yes', 'api-project-manager'),
+            'ui_off_text' => __('No', 'api-project-manager'),
+            'ui' => 1,
         ),
     ),
     'location' => array(
@@ -41,7 +62,8 @@
     'label_placement' => 'top',
     'instruction_placement' => 'label',
     'hide_on_screen' => '',
-    'active' => 1,
+    'active' => true,
     'description' => '',
+    'show_in_rest' => 0,
 ));
 }

330c01d6349d920a9a508e889a4090b68167e596
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-08-14 15:43:46 +0200

    rm theme support (to fix error)

--- source/php/Theme/Setup.php
+++ source/php/Theme/Setup.php
@@ -76,9 +76,6 @@ class Setup
      */
     public function themeSupport()
     {
-        add_theme_support('menus');
-        add_theme_support('post-thumbnails');
-        add_theme_support('html5');
         add_theme_support(
             'post-formats',
             array(
